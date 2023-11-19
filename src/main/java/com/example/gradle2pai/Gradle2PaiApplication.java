568694b3bb8c0768ac694dbee786d284702acd21
sergejsokolov <sokolov.kstovo@gmail.com>
2023-11-19 17:11:09 +0300

    Initial commit

---
+++ src/main/java/com/example/gradle2pai/Gradle2PaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle2pai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradle2PaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gradle2PaiApplication.class, args);
+    }
+
+}
